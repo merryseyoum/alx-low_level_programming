fb1b2d61b6ed2d7361153c0821841e4d4bbac717
Meron Seyoum <meronseyoum.b@gmail.com>
2022-12-08 10:10:53 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+* main - prints using the function printf
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
