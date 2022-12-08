35de300d72c407add45ed7428ca6f3b975dcb629
Meron Seyoum <meronseyoum.b@gmail.com>
2022-12-08 09:38:38 -0800

    hello world

---
+++ 4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+* main - prints using the function puts
+*
+*
+*
+*
+* Return: Always 0 (success)
+*
+**/
+
+int main(void)
+
+{
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+
+}
