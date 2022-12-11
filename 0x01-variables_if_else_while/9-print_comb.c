1d93aa101757fb0eb005d2ee0e6d209dc8e1f4c0
Meron Seyoum <meronseyoum.b@gmail.com>
2022-12-11 05:27:20 -0800

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - This program will print combination of digits
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+for (i = 0; i <= 9; i++)
+{
+putchar((i % 10) + '0');
+if (i == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
