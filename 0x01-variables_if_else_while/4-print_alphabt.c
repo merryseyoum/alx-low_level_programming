cd2a52bec9a2b2997b7422f596de9c3a90cbac25
Meron Seyoum <meronseyoum.b@gmail.com>
2022-12-11 04:29:23 -0800

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - This program will print english aplphabet
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+if (ch != 'q' && ch != 'e')
+putchar(ch);
+putchar('\n');
+return (0);
+}
