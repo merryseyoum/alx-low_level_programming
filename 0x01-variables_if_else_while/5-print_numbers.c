aa533ab2dc812bed92e5e27196abeafa8c90a423
Meron Seyoum <meronseyoum.b@gmail.com>
2022-12-11 04:33:51 -0800

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - This program will print english aplphabet
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+int i;
+for (i = 0 ; i < 10 ; i++)
+printf("%d", i);
+printf("\n");
+return (0);
+}
