3bcc402c8335fbe590d53d5313ef4221fa41e925
Meron Seyoum <meronseyoum.b@gmail.com>
2022-12-14 10:57:39 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *
+ * *main - This program will print putchar
+ *
+ * *
+ *
+ * *Return: Always 0 (success)
+ *
+ * */
+
+int main(void)
+
+{
+
+	_putchar('_');
+
+	_putchar('p');
+
+	_putchar('u');
+
+	_putchar('t');
+
+	_putchar('c');
+
+	_putchar('h');
+
+	_putchar('a');
+
+	_putchar('r');
+
+	_putchar('\n');
+
+	return (0);
+
+}
