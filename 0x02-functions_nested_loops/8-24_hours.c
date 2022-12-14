35325fbff959f79b0e9d96fa4580732392407ab4
Meron Seyoum <meronseyoum.b@gmail.com>
2022-12-14 14:25:37 -0800

    fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,22 +1,31 @@
 #include "main.h"
 /** *jack_bauer - prints the last digit * *Return: the last digits */
 
-void jack_bauer (void){ 
+void jack_bauer (void)
+{
 	int i, j;
-	for (i = 0; i < 24; i++){
-		for (j = 0; j < 60; j++){
-			if (i < 10){
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			if (i < 10)
+			{
 				_putchar ('0');
     	      			_putchar (i + '0');
-    	   		 }else if (i >= 10){
+    	   		 }
+			else if (i >= 10)
+			{
         	    		_putchar ((i / 10) + '0');
 				_putchar ((i % 10) + '0');
         		}
-        	 	if (j < 10){
+        	 	if (j < 10)
+			{
 				_putchar (':');
 				_putchar ('0');
 				_putchar (j + '0');
-        	    	}else if (j >= 10){
+        	    	}
+			else if (j >= 10)
+			{
             	  		_putchar (':');
 				_putchar ((j / 10) + '0');
 				_putchar ((j % 10) + '0');
