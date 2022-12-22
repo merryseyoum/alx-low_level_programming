fdfe3f048077ac2b54558b921ab5a50c1ac23acc
Meron Seyoum <meronseyoum.b@gmail.com>
2022-12-22 00:45:28 -0800

    fixed error

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -47,3 +47,25 @@ return (0);
 rev_string(r);
 return (r);
 }
+/**
+* rev_string - reverse array
+* @n: integer params
+* Return: 0
+*/
+void rev_string(char *n)
+{
+int i = 0;
+int j = 0;
+char temp;
+while (*(n + i) != '\0')
+{
+i++;
+}
+i--;
+for (j = 0; j < i; j++, i--)
+{
+temp = *(n + j);
+*(n + j) = *(n + i);
+*(n + i) = temp;
+}
+}
