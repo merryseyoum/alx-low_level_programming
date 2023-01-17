66a89de9fd1d813230d1bdb2c4852e288ddb448c
meronSE <meronseyoum.b@gmail.com>
2023-01-16 23:22:59 -0900

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* free_dog - frees memory created in 4-new_dog.c
+* @d: Input struct
+* Return: Nothing
+*/
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
